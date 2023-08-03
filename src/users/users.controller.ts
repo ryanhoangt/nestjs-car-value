e53ef99aef8ba817e8fb93cb5fb04c40870e76a9
Hoang Tran <descience.thh10@gmail.com>
2023-08-03 22:08:15 +0700

    create signin and signup controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -19,15 +19,24 @@ import {
   SerializeInterceptor,
 } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dto/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
   }
 
   //   @UseInterceptors(new SerializeInterceptor(UserDto))
